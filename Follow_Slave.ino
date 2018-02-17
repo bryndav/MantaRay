3ff565c63bd4099eb8b862985ad8eb82e1713d1a
David Bryngelsson <d.bryngelsson@gmail.com>
2018-02-17 22:08:03 +0100

    Major changes

--- Follow_Slave.ino
+++ Follow_Slave.ino
@@ -20,6 +20,7 @@
 ******************************************************************************/
 
 /*** Needed librarys ***/
+#include <Servo.h>
 #include <Stepper.h>
 #include <Wire.h>
 
@@ -32,40 +33,37 @@
 #define INJECTOR_OUT_POS 2048
 #define INJECTOR_IN_POS 0
 
+// Injector motor
 #define motorPin1 2
 #define motorPin2 3
 #define motorPin3 4
 #define motorPin4 5
 
-#define motorPin5 6
-#define motorPin6 7
-#define motorPin7 8
-#define motorPin8 9
+// Roll motor
+#define PWMPin1   9
 
-#define motorPin9 10
-#define motorPin10 11
-#define motorPin11 12
-#define motorPin12 13
+// Pitch motor
+#define PWMPin2   10
 
 /*** Object initialization ***/
-Stepper pitchStepper(STEPS_PER_MOTOR_REVOLUTION, motorPin1, motorPin3, motorPin2, motorPin4);
-Stepper rollStepper(STEPS_PER_MOTOR_REVOLUTION, motorPin5, motorPin7, motorPin6, motorPin8);
-Stepper injectorStepper(STEPS_PER_MOTOR_REVOLUTION, motorPin9, motorPin11, motorPin10, motorPin12);
+Servo pitchServo;
+Servo rollServo;
+Stepper injectorStepper(STEPS_PER_MOTOR_REVOLUTION, motorPin1, motorPin3, motorPin2, motorPin4);
 
 /*** Global variables ***/
 int ledVal = 0;
 
 // Current motor positions
-int pitchPos = 0;
 int heading = 0;
-int rollPos = 0;
 int injectorPos = 0;
 
 // Wanted motor positions
-int wantedPitchPos = 0;
 int wantedHeading = 0;
-int wantedRoll = 0;
 int wantedInjectorPos = 0;
+
+// Servo output
+int pitchOutput = 0;
+int rollOutput = 0;
 
 void setup(){
     Wire.begin(15);                 // Initializes I2C as slave with adress 15
@@ -74,16 +72,13 @@ void setup(){
     pinMode(LED, OUTPUT);
 
     // Motor configurations
+    rollServo.attach(PWMPin1);
+    pitchServo.attach(PWMPin2);
+
     pinMode(motorPin1, OUTPUT);
     pinMode(motorPin2, OUTPUT);
     pinMode(motorPin3, OUTPUT);
     pinMode(motorPin4, OUTPUT);
-    pinMode(motorPin5, OUTPUT);
-    pinMode(motorPin6, OUTPUT);
-    pinMode(motorPin7, OUTPUT);
-    pinMode(motorPin8, OUTPUT);
-    pitchStepper.setSpeed(700);
-    rollStepper.setSpeed(700);
     injectorStepper.setSpeed(700);
 
     Serial.begin(9600);
@@ -92,30 +87,15 @@ void setup(){
 void loop(){
 
     // If a wanted position does not correspond with a wanted one. Step said motor one step
-    if(pitchPos != wantedPitchPos){
-        if (pitchPos > wantedPitchPos){
-            pitchStepper.step(-1);
-            pitchPos = pitchPos - 1;
-        }else{
-            pitchStepper.step(1);
-            pitchPos = pitchPos + 1;
-        }
-    }
-
-    if(rollPos != wantedRoll){
-        if(rollPos > wantedRoll){
-            rollStepper.step(-1);
-            rollPos = rollPos - 1;
-        }else{
-            rollStepper.step(1);
-            rollPos = rollPos + 1;
-        }
-    }
-
    if (injectorPos != wantedInjectorPos){
       injectorStepper.step(1);
       injectorPos = injectorPos + 1;
    }
+
+   rollServo.write(rollOutput);
+   delay(10);
+   pitchServo.write(pitchOutput);
+   delay(10); 
 }
 
 void receiveEvent (int length){
@@ -131,8 +111,7 @@ void receiveEvent (int length){
         digitalWrite (LED, ledVal ^= 1);
 
         while (Wire.available() > 0) // Gets rid of any spare junk on the I2C wire
-            Wire.read();
-        
+            Wire.read();       
     }
 
 }
@@ -141,18 +120,30 @@ void decodeMSG(int val){
     // The encoding is done by adding either 2000, 4000, 6000 or 8000
     // By looking on the size of the value we can then decide which value to alter
     if (val < 3000){
-        wantedHeading = val - 2000;
+        wantedHeading = (val - 2000);
         wantedHeading = map(wantedHeading, 0, 360, 0,2048);
+        Serial.print("Recieved new heading: ");
+        Serial.println(val - 2000);
     }else if((val >= 3000) && (val < 5000)){
-        wantedPitchPos = val - 4000;
-        wantedPitchPos = map(wantedPitchPos, -179, 179, -5120, 5120);
+        pitchOutput = (val - 4000);
+        pitchOutput = map(pitchOutput, -179, 179, 0, 180);
+        Serial.print("Recieved new pitch value: ");
+        Serial.println(val - 4000);
+        Serial.print("New pitch output: ");
+        Serial.println(pitchOutput);
     }else if((val >= 5000) && (val < 8000)){
-        wantedRoll = val - 6000;
-        wantedRoll = map(wantedRoll, 0, 90, 0, 2048);
-    }else if(val >= 8000){   
+        rollOutput = (val - 6000);
+        rollOutput = map(rollOutput, -90, 90, 0, 180);
+        Serial.print("Recieved new roll value: ");
+        Serial.println(val - 6000);
+        Serial.print("New roll output: ");
+        Serial.println(rollOutput);
+    }else if(val >= 8000){ 
+        Serial.print("Recieved new potentiometer value: ");
+        Serial.println(val - 8000);  
         if((val - 8000) == 0){
            wantedInjectorPos = INJECTOR_IN_POS;
-        }else if((val - 8000) == 688){
+        }else if((val - 8000) == 690){
            wantedInjectorPos = INJECTOR_OUT_POS;  
         }
     }
